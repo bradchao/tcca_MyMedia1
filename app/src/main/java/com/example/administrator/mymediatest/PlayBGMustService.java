b03bb8443834a7a93cce0b303d513171c07c1536
Brad Chao <brad@brad.tw>
2017-11-05 09:13:10 +0800

    v2

--- app/src/main/java/com/example/administrator/mymediatest/PlayBGMustService.java
+++ app/src/main/java/com/example/administrator/mymediatest/PlayBGMustService.java
@@ -2,10 +2,12 @@ package com.example.administrator.mymediatest;
 
 import android.app.Service;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Binder;
 import android.os.IBinder;
 
 public class PlayBGMustService extends Service {
+    private MediaPlayer mediaPlayer;
     private final LocalBinder mBinder = new LocalBinder();
 
     public class LocalBinder extends Binder {
@@ -22,10 +24,14 @@ public class PlayBGMustService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
+        mediaPlayer = MediaPlayer.create(this, R.raw.brad);
+        mediaPlayer.setLooping(true);
+        mediaPlayer.start();
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
+        mediaPlayer.stop();
     }
 }
